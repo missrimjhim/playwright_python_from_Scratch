362ad276a1bbc692402c33b4d1a19f45678ff16e
missrimjhim <arpitamarch092@gmail.com>
2026-02-17 15:23:48 +0530

    test file code updated

--- tests/test_auth_testcases.py
+++ tests/test_auth_testcases.py
@@ -1,10 +1,12 @@
 from pages.login_page import LoginPage
-
+from utils.html_logger import HTMLLogger
 
 def test_TC_01_User_registration(browser_context):
-    login_page = LoginPage(browser_context)
-    login_page.register_first_time("abcde", "efgd","asdfgh","543eashgfgdyudd@gmail.com")
+    logger = HTMLLogger()
+    login_page = LoginPage(browser_context,logger)
+    login_page.register_first_time("abcde", "efgd","asdfgh","54389y@gmail.com")
     login_page.validate_registration()
+    logger.save()
 
 
 
